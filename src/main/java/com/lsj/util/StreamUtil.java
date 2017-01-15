45af00ced0a3544179aa152a6be66b653148d7af
lsj <lsj9383@163.com>
2017-01-15 20:21:06 +0800

    fix a bug

--- src/main/java/com/lsj/util/StreamUtil.java
+++ src/main/java/com/lsj/util/StreamUtil.java
@@ -1,16 +1,17 @@
 package com.lsj.util;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 public class StreamUtil {
 	static public String ReadInputStream(InputStream is) throws Exception{
+		InputStreamReader isr = new InputStreamReader(is);
 		StringBuilder text = new StringBuilder();
-		byte[] buffer = new byte[is.available()];
+		char[] buffer = new char[1024];
 		int length = 0;
 		
-		while(((length = is.read(buffer)) != -1)
-			&& (buffer.length != 0)){
+		while((length = isr.read(buffer)) != -1){
 			text.append(new String(buffer, 0, length));
 		}		
 		
@@ -22,10 +23,9 @@ public class StreamUtil {
 	}
 	
 	static public void InputStream2OutputStream(InputStream is, OutputStream os) throws Exception{
-		byte[] buffer = new byte[is.available()];
+		byte[] buffer = new byte[1024];
 		int length = 0;
-		while(((length = is.read(buffer)) != -1)
-			&& buffer.length != 0){
+		while((length = is.read(buffer)) != -1){
 			os.write(buffer, 0, length);
 		}
 	}
